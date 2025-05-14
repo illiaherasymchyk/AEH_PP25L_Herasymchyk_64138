e2e5f3d2950e153400a683d370210dbc153a25d1
illiaherasymchyk <herasymchykillia@ukr.net>
2025-05-14 14:10:53 +0200

    Update mojaOsmaAplikacija.java

--- src/pl/pp/mojaSiodmaAplikacija.java
+++ src/pl/pp/mojaSiodmaAplikacija.java
@@ -1,14 +1,19 @@
 package pl.pp;
 
-public class mojaSiodmaAplikacija {
+public class mojaOsmaAplikacja {
     public static void main(String[] args) {
-
-        Person person1 = new Person("Mateusz", "Karmazyn", 24, "Warszawa", 1997);
-
-
-        person1.growOld(10);
-
-
-        person1.hiToAll();
+        Konto konto1 = new Konto(
+                "1234567890",
+                1000.0,
+                "Jan Kowalski",
+                "jan@example.com",
+                "123456789"
+        );
+        System.out.println();
+        // Przykładowe operacje
+        konto1.wyplata(900.0);
+        konto1.wplata(250.0);
+        konto1.wyplata(50.0);
+        konto1.wyplata(500.0);
     }
 }
